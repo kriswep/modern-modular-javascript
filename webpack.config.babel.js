57de94982759af2da348f474639a6a47900c6e70
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-03-11 21:23:28 +0100

    prevent babel to transform commonjs

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,41 +6,43 @@ const production = process.env.NODE_ENV === 'production';
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
 
-export default {
-  entry: `${APP_DIR}/client/app.js`,
-  output: {
-    path: BUILD_DIR,
-    filename: 'client-bundle.js',
-  },
-  devtool: production ? false : 'sourcemap',
-  devServer: {
-    hot: true,
-    inline: true,
-    contentBase: BUILD_DIR,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { modules: 'true' }
-          },
-        ],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env'],
+module.exports = function config() {
+  return {
+    entry: `${APP_DIR}/client/app.js`,
+    output: {
+      path: BUILD_DIR,
+      filename: 'client-bundle.js',
+    },
+    devtool: production ? false : 'sourcemap',
+    devServer: {
+      hot: true,
+      inline: true,
+      contentBase: BUILD_DIR,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { modules: 'true' }
+            },
+          ],
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env'],
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
 };
